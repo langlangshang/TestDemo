5737834405de01265356226ddf034bf21efb5a41
langlangshang <1206728627@qq.com>
2019-06-29 14:24:55 +0800

    第四次

--- TestDemo/src/main/java/com/lichuan/entity/User.java
+++ TestDemo/src/main/java/com/lichuan/entity/User.java
@@ -2,7 +2,8 @@ package com.lichuan.entity;
 
 public class User {
 private String name;
-private String password;
+private String lichuan;
+private String aaa;
 private Integer age;
 public String getName() {
 	return name;
@@ -10,15 +11,25 @@ public String getName() {
 public void setName(String name) {
 	this.name = name;
 }
-public String getPassword() {
-	return password;
+public String getLichuan() {
+	return lichuan;
 }
-public void setPassword(String password) {
-	this.password = password;
+public void setLichuan(String lichuan) {
+	this.lichuan = lichuan;
 }
+public String getAaa() {
+	return aaa;
+}
+public void setAaa(String aaa) {
+	this.aaa = aaa;
+}
+
+
+
 public Integer getAge() {
 	return age;
 }
+
 public void setAge(Integer age) {
 	this.age = age;
 }
